f1f5f9e5bbf6d69aa2bbe15dd56354fc3c80d3dd
Trevor Kimenye <trevor@kimenye.com>
2012-06-22 16:23:57 +0300

    Changed the application to use the modular sinatra pattern as well as done some rspec testing

--- api_application.rb
+++ api_application.rb
@@ -1,7 +1,6 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/json'
-require 'sinatra/advanced_routes'
 require 'sinatra/reloader' if development?
 require 'dm-core'
 require 'mongo_mapper'
@@ -22,76 +21,78 @@ class TvShow
   key :created_at, Time, :default => Time.now
 end
 
-configure do
-  set :public_folder, Proc.new { File.join(root, "static") }
-  MongoMapper.database = 'tivi'
-  enable :sessions
-end
-
-get '/' do
-  haml :home, :layout => :index
-end
-
-before '/api/*' do
-  content_type :json
-end
-
-
-get '/api/describe' do
-  body({ version: "1.0 "}.to_json)
-end
-
-post '/api/channel' do
-  data = JSON.parse(request.body.string)
-  if data.nil? or !data.has_key?('name') or !data.has_key?('code')
-    status 400
-  else
-    channel = Channel.new
-    channel.name = data['name']
-    channel.code = data['code']
-    channel.save!
-    status 200
-    body(channel.id.to_s)
+class ApiApplication < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
   end
-end
 
-get '/api/channel/:id' do
-  channel = Channel.find(params[:id])
-  body(channel.to_json)
-end
-
-get '/api/channels' do
-  channels = Channel.all
-  body(channels.to_json)
-end
-
-
-
-#create a tv show
-post '/api/show' do
-  data = JSON.parse(request.body.string)
-  if data.nil?  or !data.has_key?('name') or !data.has_key?('description') then
-    status 400
-  else
-    show = TvShow.new
-    show.description = data['description']
-    show.name = data['name']
-    show.save!
-    status 200
-    body(show.id.to_s)
+  configure do
+    set :public_folder, Proc.new { File.join(root, "static") }
+    MongoMapper.database = 'tivi'
+    enable :sessions
   end
+
+  get '/' do
+    haml :home, :layout => :index
+  end
+
+  before '/api/*' do
+    content_type :json
+  end
+
+  get '/api/describe' do
+    body({ version: "1.0 "}.to_json)
+  end
+
+  post '/api/channel' do
+    data = JSON.parse(request.body.string)
+    if data.nil? or !data.has_key?('name') or !data.has_key?('code')
+      status 400
+    else
+      channel = Channel.new
+      channel.name = data['name']
+      channel.code = data['code']
+      channel.save!
+      status 200
+      body(channel.id.to_s)
+    end
+  end
+
+  get '/api/channel/:id' do
+    channel = Channel.find(params[:id])
+    body(channel.to_json)
+  end
+
+  get '/api/channels' do
+    channels = Channel.all
+    body(channels.to_json)
+  end
+
+  #create a tv show
+  post '/api/show' do
+    data = JSON.parse(request.body.string)
+    if data.nil?  or !data.has_key?('name') or !data.has_key?('description') then
+      status 400
+    else
+      show = TvShow.new
+      show.description = data['description']
+      show.name = data['name']
+      show.save!
+      status 200
+      body(show.id.to_s)
+    end
+  end
+
+  get '/api/shows' do
+    shows = TvShow.all
+    body(shows.to_json)
+  end
+
+  get '/api/show/:id' do
+    show = TvShow.find(params[:id])
+    body(show.to_json)
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
-
-get '/api/shows' do
-  shows = TvShow.all
-  body(shows.to_json)
-end
-
-get '/api/show/:id' do
-  show = TvShow.find(params[:id])
-  body(show.to_json)
-end
-
-
-
-
--- tests/api.rb
+++ tests/api.rb
@@ -1,5 +1,6 @@
 require_relative '../api_application'  # <-- your sinatra app
 require 'spec/mocks'
+require 'rspec-expectations'
 require 'rack/test'
 
 set :environment, :test
@@ -8,12 +9,44 @@ describe 'The Tivi App' do
   include Rack::Test::Methods
 
   def app
-    Sinatra::Application
+    ApiApplication
   end
 
   it "returns the correct version of the api" do
     get '/api/describe'
     last_response.should be_ok
     last_response.body.should == "{\"version\":\"1.0 \"}"
+  end
+
+  it "returns tv channels" do
+    get '/api/channels'
+    channels = Channel.all
+    last_response.should be_ok
+    last_response.body.should == channels.to_json
+  end
+
+  it "returns a tv channel" do
+    channel = Channel.first()
+    if !channel.nil?
+      get "/api/channel/#{channel.id}"
+      last_response.should be_ok
+      last_response.body.should == channel.to_json
+    end
+  end
+
+  it "returns tv shows" do
+    get '/api/shows'
+    shows = TvShow.all
+    last_response.should be_ok
+    last_response.body.should == shows.to_json
+  end
+  
+  it "returns a tv show" do
+    show = TvShow.first()
+    if !show.nil?
+      get "/api/show/#{show.id}"
+      last_response.should be_ok
+      last_response.body.should == show.to_json
+    end  
   end
 end
