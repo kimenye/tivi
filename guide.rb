de8f4387aa57226c2df8b8ca1af07ef5f44133c6
Joyce Echessa <jokhessa@yahoo.com>
2012-11-07 05:27:54 +0300

    Continued working on the mobile guide

--- guide.rb
+++ guide.rb
@@ -3,23 +3,28 @@ require 'sinatra'
 require 'pry'
 require 'haml'
 require 'time'
-require_relative 'api_application'
 require 'sinatra/reloader' if development? or test?
 
 class GuideApp < Sinatra::Base
 
-  get '/mobile' do
+  helpers SchedulerHelper
 
+  get '/' do
 
-
-  end
-
-  get '/mobile/channel/:id' do
-
+    channel_summary = Array.new
+    channel_complete = Array.new
+    show_description = Array.new
+    channels = Channel.all
+    channels.each do |channel|
+      current_and_next_schedule = get_current_and_next_schedule(channel)
+      channel_summary.push(current_and_next_schedule)
+      schedule_for_rest_of_day = get_schedule_for_rest_of_day(channel)
+      channel_complete.push(schedule_for_rest_of_day)
+    end
     channel = Channel.find(params[:id])
     schedule = get_schedule_for_rest_of_day(channel)
 
-    haml :"admin/channel", :layout => :admin, :locals => {:channel => channel, :schedule => schedule, :day => day, :prev_day => prev_day, :next_day => next_day }
+    haml :"guide/mobile", :layout => :guide_layout, :locals => {:channel_summary => channel_summary, :channel_complete => channel_complete, :show_description => show_description }
 
   end
   
