883115b6f8625566a1539ddcf0c7adc8499e33a1
Joyce Echessa <jokhessa@yahoo.com>
2012-11-08 15:19:06 +0300

    Continued working on the mobile guide

--- guide.rb
+++ guide.rb
@@ -13,18 +13,22 @@ class GuideApp < Sinatra::Base
 
     channel_summary = Array.new
     channel_complete = Array.new
-    show_description = Array.new
     channels = Channel.all
     channels.each do |channel|
+      temp_hash1 = Hash.new
+      temp_hash2 = Hash.new
       current_and_next_schedule = get_current_and_next_schedule(channel)
-      channel_summary.push(current_and_next_schedule)
       schedule_for_rest_of_day = get_schedule_for_rest_of_day(channel)
-      channel_complete.push(schedule_for_rest_of_day)
+      temp_hash1["channel"] = channel
+      temp_hash1["shows"] = current_and_next_schedule
+      channel_summary.push(temp_hash1)
+      temp_hash2["channel"] = channel
+      temp_hash2["shows"] = schedule_for_rest_of_day
+      channel_complete.push(temp_hash2)
     end
-    channel = Channel.find(params[:id])
-    schedule = get_schedule_for_rest_of_day(channel)
+    # binding.pry
 
-    haml :"guide/mobile", :layout => :guide_layout, :locals => {:channel_summary => channel_summary, :channel_complete => channel_complete, :show_description => show_description }
+    haml :"guide/mobile", :layout => :guide_layout, :locals => {:channel_summary => channel_summary, :channel_complete => channel_complete }
 
   end
   
