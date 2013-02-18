4d974e2324bf418f8fa24929fbfc92d36afdc592
Trevor Kimenye <kimenye@gmail.com>
2013-02-18 16:50:31 +0300

    Using new liquid slider

--- static/javascript/guide.js
+++ static/javascript/guide.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
     function EmbeddedApp() {
         var self = this;
-        this.channels = ko.observable([]);
+        this.channels = ko.observableArray([]);
         this.loading = ko.observable(true);
+        this.slideIdx = ko.observable(0);
+        this.current = ko.computed(function() {
+            if (self.channels().length > 0)
+                return self.channels()[self.slideIdx()].code;
+            else
+                return ""
+        });
 
         $.getJSON("/api/guide", function(data) {
             _.each(data, function(c) {
-                self.channels().push(new Channel(c));
+                self.channels.push(new Channel(c));
             });
+
+
 
             self.loading(false);
         });
