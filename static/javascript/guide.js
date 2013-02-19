4345ff3999aa848835359eb2df04671a76c1d76d
Joyce Echessa <jokhessa@yahoo.com>
2013-02-19 12:34:04 +0300

    Added function to get Channel shows

--- static/javascript/guide.js
+++ static/javascript/guide.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     function EmbeddedApp() {
         var self = this;
-        this.channels = ko.observable([]);
+        this.channels = ko.observableArray([]);
         this.loading = ko.observable(true);
 
         $.getJSON("/api/guide", function(data) {
             _.each(data, function(c) {
-                self.channels().push(new Channel(c));
+                self.channels.push(new Channel(c));
             });
 
             self.loading(false);
@@ -19,10 +19,35 @@ $(document).ready(function() {
 
 
     function Channel(data) {
+        var self = this;
         var json = $.parseJSON(data.channel);
         this.code = json.code;
         this.name = json.name;
         this.logo = json.logo_id;
+
+        this.currentShow = ko.observable();
+        this.nextShow = ko.observable();
+        this.restOfShows = ko.observableArray([]);
+
+        self.currentShow(new Show($.parseJSON(data.current)));
+        self.nextShow(new Show($.parseJSON(data.next)));
+
+        var rest = $.parseJSON(data.rest);
+
+        for (var i in rest) {
+            var show = new Show(rest[i]);
+            self.restOfShows.push(show);
+        }
+    }
+
+    function Show(data) {
+        this.start_time = data.start_time;
+        this.end_time = data.end_time;
+        this.promo_text = data.promo_text;
+        var show = $.parseJSON(data.show);
+        this.name = show.name;
+        this.logo_id = show.logo_id;
+        this.description = show.description;
     }
 
     function show() {
@@ -31,4 +56,5 @@ $(document).ready(function() {
     }
 
     ko.applyBindings(new EmbeddedApp());
+
 });
