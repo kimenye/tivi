e4191ed20a20c46f0997c35744193f6518774eab
Trevor Kimenye <kimenye@gmail.com>
2013-02-27 18:43:01 +0300

    Media server puts

--- media.rb
+++ media.rb
@@ -4,11 +4,14 @@ require 'rack/gridfs'
 
 class MediaApp < Sinatra::Base
   db = 'tivi'
+
   if ENV['MONGOHQ_URL']
     uri = URI.parse(ENV['MONGOHQ_URL'])
     MongoMapper.connection = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
     db = uri.path.gsub(/^\//, '')
   end
+
+  puts ">>> DB is #{db}"
 
   use Rack::GridFS, :database => db, :prefix => 'images'
 
