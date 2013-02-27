df80c93e7157bfd5a2cb30b255f99f845b6bf90c
Trevor Kimenye <kimenye@gmail.com>
2013-02-27 19:20:45 +0300

    changed rackfs url

--- media.rb
+++ media.rb
@@ -7,16 +7,22 @@ class MediaApp < Sinatra::Base
     db = 'tivi'
 
     #binding.pry
+    uri = nil
     if ENV['MONGOHQ_URL']
       uri = URI.parse(ENV['MONGOHQ_URL'])
       puts "URL : >>> #{uri}"
       MongoMapper.connection = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
       db = uri.path.gsub(/^\//, '')
+
     end
 
     puts ">>> DB is #{db}"
 
-    use Rack::GridFS, :database => db, :prefix => 'images'
+    if production?
+      use Rack::GridFS, :database => db, :hostname => uri.host, :password => uri.password, :port => uri.port, :username => uri.user, :prefix => 'images'
+    else
+      use Rack::GridFS, :database => db, :prefix => 'images'
+    end
   end
 
   get /.*/ do
