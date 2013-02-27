0d9a9ded7e030978c08a38794ed3010b0a42db64
Trevor Kimenye <kimenye@gmail.com>
2013-02-27 19:28:09 +0300

    Added spacing for images

--- media.rb
+++ media.rb
@@ -6,18 +6,13 @@ class MediaApp < Sinatra::Base
   configure do
     db = 'tivi'
 
-    #binding.pry
     uri = nil
     if ENV['MONGOHQ_URL']
       uri = URI.parse(ENV['MONGOHQ_URL'])
       puts "URL : >>> #{uri}"
       MongoMapper.connection = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
       db = uri.path.gsub(/^\//, '')
-
     end
-
-    puts ">>> DB is #{db}"
-
     if production?
       use Rack::GridFS, :database => db, :hostname => uri.host, :password => uri.password, :port => uri.port, :username => uri.user, :prefix => 'images'
     else
