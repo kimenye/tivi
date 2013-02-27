e83771dfe77b0a426979de597934de6c71be1acc
Trevor Kimenye <kimenye@gmail.com>
2013-02-27 18:48:27 +0300

    Moved setup to configure

--- media.rb
+++ media.rb
@@ -3,18 +3,21 @@ require 'sinatra'
 require 'rack/gridfs'
 
 class MediaApp < Sinatra::Base
-  db = 'tivi'
+  configure do
+    db = 'tivi'
 
-  binding.pry
-  if ENV['MONGOHQ_URL']
-    uri = URI.parse(ENV['MONGOHQ_URL'])
-    MongoMapper.connection = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
-    db = uri.path.gsub(/^\//, '')
+    binding.pry
+    if ENV['MONGOHQ_URL']
+      uri = URI.parse(ENV['MONGOHQ_URL'])
+      puts "URL : >>> #{uri}"
+      MongoMapper.connection = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+      db = uri.path.gsub(/^\//, '')
+    end
+
+    puts ">>> DB is #{db}"
+
+    use Rack::GridFS, :database => db, :prefix => 'images'
   end
-
-  puts ">>> DB is #{db}"
-
-  use Rack::GridFS, :database => db, :prefix => 'images'
 
   get /.*/ do
     "The URL did not match a file in GridFS."
